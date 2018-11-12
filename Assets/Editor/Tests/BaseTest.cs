2ed403c01cbefa40e3ce71b86ab642ea9e81fa87
Daniel Parra <dani.parra.90@gmail.com>
2018-11-12 22:28:15 +0100

    Add move system test

---
+++ Assets/Editor/Tests/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Unity.Entities;
+
+namespace Editor.Tests
+{
+    public class BaseTest
+    {
+        protected World World;
+        protected EntityManager EntityManager;
+        protected EntityManager.EntityManagerDebug ManagerDebug;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            World = new World("Test World");
+
+            EntityManager = World.GetOrCreateManager<EntityManager>();
+            ManagerDebug = new EntityManager.EntityManagerDebug(EntityManager);
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            if (EntityManager != null)
+            {
+                World.Dispose();
+                World = null;
+
+                EntityManager = null;
+            }
+        }
+    }
+}
---
+++ Assets/Editor/Tests/Systems/Move/MoveTest.cs
@@ -0,0 +1,30 @@
+﻿using Components.Common;
+using NUnit.Framework;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Editor.Tests.Systems.Move
+{
+    [TestFixture]
+    public class MoveTest : BaseTest
+    {
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(3)]
+        public void MoveEntity(int speed)
+        {
+            // Arrange
+            var entity = EntityManager.CreateEntity(typeof(Position2D),
+                typeof(Heading2D),
+                typeof(MoveSpeed));
+            EntityManager.SetComponentData(entity, new Heading2D {Value = new float2 {x = 1, y = 0}});
+            EntityManager.SetComponentData(entity, new MoveSpeed {Value = speed});
+
+            // Act
+            World.CreateManager<global::Systems.Common.Move>().Update();
+            
+            // Assert
+            Assert.AreEqual(new float2 {x = speed * Time.deltaTime, y = 0}, EntityManager.GetComponentData<Position2D>(entity).Value);
+        }
+    }
+}
