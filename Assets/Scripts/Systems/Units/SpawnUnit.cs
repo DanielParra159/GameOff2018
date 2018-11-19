c456c5c24a43ecdd6eb93912d949ccf0380386b8
Daniel Parra <dani.parra.90@gmail.com>
2018-11-19 22:15:35 +0100

    Update energy when spawn a unit

--- Assets/Scripts/Systems/Units/SpawnUnit.cs
+++ Assets/Scripts/Systems/Units/SpawnUnit.cs
@@ -1,12 +1,22 @@
+using Components.Common;
+using Components.Player;
 using Components.Units;
 using JetBrains.Annotations;
 using Unity.Entities;
+using UnityEngine.Assertions;
 
 namespace Systems.Units
 {
     [UsedImplicitly]
     public class SpawnUnit : ComponentSystem
     {
+        private struct PlayersData
+        {
+            public ComponentDataArray<Faction> Factions;
+            public ComponentDataArray<Energy> Energies;
+        }
+        
+        [Inject] private PlayersData _playerDataGroup;
         private ComponentGroup _spawnInfoGroup;
 
         protected override void OnCreateManager()
@@ -21,7 +31,20 @@ namespace Systems.Units
 
             for (var i = 0; i < entities.Length; ++i)
             {
-                SceneInitializer.Instance.UnitFactory.Instance(spawnsInfo[i]);
+                var spawnInfo = spawnsInfo[i];
+
+                for (var playerIndex = 0; playerIndex < _playerDataGroup.Factions.Length; ++playerIndex)
+                {
+                    if (_playerDataGroup.Factions[playerIndex].Value != spawnInfo.Faction)
+                        continue;
+
+                    var energy = _playerDataGroup.Energies[playerIndex];
+                    Assert.IsTrue(energy.CurrentValue >= spawnInfo.Energy, "Don't have enough energy");
+                    energy.CurrentValue -= spawnInfo.Energy;
+                    _playerDataGroup.Energies[playerIndex] = energy;
+                }
+
+                SceneInitializer.Instance.UnitFactory.Instance(spawnInfo);
                 PostUpdateCommands.DestroyEntity(entities[i]);
             }
         }
